298a6af8a0e8bd78eb6665988306ab8ace4c3966
Nikolay Kim <fafhrd91@gmail.com>
2017-12-19 09:09:18 -0800

    update actix example

--- actix-hello/src/main.rs
+++ actix-hello/src/main.rs
@@ -10,7 +10,8 @@ fn main() {
         || Application::default()
             .resource("/", |r| r.f(|_| HttpResponse::Ok().body("Hello World!"))))
         .threads(1)
-        .serve::<_, ()>("127.0.0.1:8080").unwrap();
+        .bind("127.0.0.1:8080").unwrap()
+        .start();
 
     println!("Started http server: 127.0.0.1:8080");
     let _ = sys.run();
