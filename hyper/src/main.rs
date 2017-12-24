06c91facb45ea0ee6909e3daabd385abef7cac93
Nikolay Kim <fafhrd91@gmail.com>
2017-12-24 15:14:26 -0800

    add hyper 0.11

---
+++ hyper/src/main.rs
@@ -0,0 +1,24 @@
+#![deny(warnings)]
+extern crate hyper;
+extern crate futures;
+
+use hyper::header::{ContentLength, ContentType};
+use hyper::server::{Http, Response, const_service, service_fn};
+
+static PHRASE: &'static [u8] = b"Hello World!";
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 8080).into();
+
+    let new_service = const_service(service_fn(|_| {
+        Ok(Response::<hyper::Body>::new()
+            .with_header(ContentLength(PHRASE.len() as u64))
+            .with_header(ContentType::plaintext())
+            .with_body(PHRASE))
+    }));
+
+    let mut server = Http::new().bind(&addr, new_service).unwrap();
+    server.no_proto();
+    println!("Listening on http://{} with 1 thread.", server.local_addr().unwrap());
+    server.run().unwrap();
+}
