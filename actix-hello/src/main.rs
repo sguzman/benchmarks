f03cbf5e08707534ccf6ce5dfb005543efb3f127
Nikolay Kim <fafhrd91@gmail.com>
2018-02-19 17:17:49 -0800

    use actix-web 0.4 from master

--- actix-hello/src/main.rs
+++ actix-hello/src/main.rs
@@ -8,7 +8,8 @@ fn main() {
 
     HttpServer::new(
         || Application::default()
-            .resource("/", |r| r.f(|_| HttpResponse::Ok().body("Hello World!"))))
+            .handler("/", |_|
+                HttpResponse::Ok().content_type("text/plain; charset=utf-8").body("Hello World!")))
         .threads(1)
         .bind("127.0.0.1:8080").unwrap()
         .start();
